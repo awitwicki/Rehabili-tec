3010ec08fe4948bc3e731e8d526275bb330cc9c4
lishnor <lisznor@gmail.com>
2019-03-23 13:05:33 +0100

    Platform Detection

---
+++ Unity/Rehabili-tec/Assets/PlayerPlatformDetection.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPlatformDetection : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+            other.GetComponent<PlatformPositionDetector>().PlayerIn = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Platform")
+        {
+            other.GetComponent<PlatformPositionDetector>().PlayerIn = false;
+        }
+    }
+}
---
+++ Unity/Rehabili-tec/Assets/Scripts/PlatformPositionDetector.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformPositionDetector : MonoBehaviour
+{
+    public MeshRenderer lightIndicator;
+    private bool playerIn;
+    public bool PlayerIn
+    { get
+        {
+            return playerIn;
+        }
+      set
+        {
+            UpdateStatus(value);
+            playerIn = value;
+        }
+    }
+
+    private void UpdateStatus(bool status)
+    {
+        if (status)
+        {
+            Debug.Log("dziala", gameObject);
+            lightIndicator.material.SetColor("_EmissionColor", Color.green);
+        }
+        else
+            lightIndicator.material.SetColor("_EmissionColor", Color.red);
+    }
+}
